13a9cc89bf3fae211f0bdc4b16455a49610b3a1a
petermec <petermec@gmail.com>
2015-06-11 09:00:58 +0200

    final submit
    removed clutter from folder, checked files

--- test.R
+++
@@ -1,4 +0,0 @@
-url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
-download.file(url, destfile="StormData.csv.bz2")
-## No need to decompress this downloaded file.
-stormData <- read.csv("StormData.csv.bz2", stringsAsFactors=F)
